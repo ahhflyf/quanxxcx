2cac201d15877561580364765af935dd8e37f1ff
ahhflyf <ahhflyf@qq.com>
2024-07-31 18:47:58 +0800

    Update getxcxbearer.js

--- getxcxbearer.js
+++ getxcxbearer.js
@@ -4,21 +4,21 @@ hostname = member.imixpark.com, mina-cmapi.cp-properties.cn, weixinnew.lalaport-
 
 
 # lalaport cookie
-^https\:\/\/weixinnew\.lalaport\-jq\.com\:42211\/api\/Sign\/SignIn url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/lalaGetBearer.js
+^https\:\/\/weixinnew\.lalaport\-jq\.com\:42211\/api\/Sign\/SignIn url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/lalaGetBearer.js?token=GHSAT0AAAAAACUQ2OKFBULP7V22SZRQAYA4ZVKCZAQ
 
 # 船厂 cookie
-^https\:\/\/mina\-cmapi\.cp\-properties\.cn\:1443\/api\/VipInfo\/Sign url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/cmapiGetBearer.js
+^https\:\/\/mina\-cmapi\.cp\-properties\.cn\:1443\/api\/VipInfo\/Sign url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/cmapiGetBearer.js?token=GHSAT0AAAAAACUQ2OKF5TJOEZ5WWK5EA7KQZVKCZXQ
 
 # 大融城 cookie
-^https\:\/\/member\.imixpark\.com\:48889\/api\/Sign\/SignIn  url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/drcGetBearer.js
+^https\:\/\/member\.imixpark\.com\:48889\/api\/Sign\/SignIn  url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/drcGetBearer.js?token=GHSAT0AAAAAACUQ2OKF5TJOEZ5WWK5EA7KQZVKCZXQ
 
 # lcm cookie
-^https\:\/\/vipclub\.lcmsh\.com\:52501\/api\/Sign\/SignIn url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/lcmGetBearer.js
+^https\:\/\/vipclub\.lcmsh\.com\:52501\/api\/Sign\/SignIn url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/lcmGetBearer.js?token=GHSAT0AAAAAACUQ2OKF5TJOEZ5WWK5EA7KQZVKCZXQ
 
 # wh cookie
-^https\:\/\/www\.kezaihui\.com\/saintpeter\/user\/business_groups\/0fb2d1dbebc04b9ca376a0265f27539f\/user_checkin_info url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/whGetBearer.js
+^https\:\/\/www\.kezaihui\.com\/saintpeter\/user\/business_groups\/0fb2d1dbebc04b9ca376a0265f27539f\/user_checkin_info url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/whGetBearer.js?token=GHSAT0AAAAAACUQ2OKF5TJOEZ5WWK5EA7KQZVKCZXQ
 
 # 新达汇 cookie
-^https\:\/\/wox2019\.woxshare\.com\/clientApi\/popupList url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/xdhGetBearer.js
-^https\:\/\/wox2019\.woxshare\.com\/clientApi\/popupList url script-request-body https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/xdhGetBearerbody.js
+#^https\:\/\/wox2019\.woxshare\.com\/clientApi\/popupList url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/xdhGetBearer.js
+#^https\:\/\/wox2019\.woxshare\.com\/clientApi\/popupList url script-request-body https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/xdhGetBearerbody.js
 
