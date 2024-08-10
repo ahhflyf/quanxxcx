46696afc53e3ca927c4984050d3380ad1de4216f
ahhflyf <ahhflyf@qq.com>
2024-08-10 14:34:47 +0800

    Update getxcxbearer.js

--- getxcxbearer.js
+++ getxcxbearer.js
@@ -18,8 +18,8 @@ hostname = member.imixpark.com, mina-cmapi.cp-properties.cn, weixinnew.lalaport-
 # wh cookie
 #^https\:\/\/www\.kezaihui\.com\/saintpeter\/user\/business_groups\/0fb2d1dbebc04b9ca376a0265f27539f\/user_checkin_info url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/whGetBearer.js
 
-#森兰
-^https\:\/\/sunland\.cjtzn\.com\/api\/user\/memberSign\/sign url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/getslcheckin.sj
+# 森兰
+^https\:\/\/sunland\.cjtzn\.com\/api\/user\/memberLevel\/getMemberLevelList url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/getslcheckin.sj
 
 # 新达汇 cookie
 #^https\:\/\/wox2019\.woxshare\.com\/clientApi\/popupList url script-request-header https://raw.githubusercontent.com/ahhflyf/quanxxcx/main/xdhGetBearer.js
